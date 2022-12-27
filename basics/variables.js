8a668c659de7a6a38ac68487601a74170ba23589
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2022-12-27 13:46:30 +0000

    printed vales in tabler formate..

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "abcd@gmail.com"
+let accountPassword = "abcd123"
+accountCity = "jorhat"
+
+// accountId = 2
+accountEmail = "aabbc@gmail.com"
+accountPassword = "ababava"
+accountCity = "guwahati"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/*
+**Prefer Not to use Var Keyword To declear the variables
+**Because of the issue occure in block scope and functional scope
+*/
