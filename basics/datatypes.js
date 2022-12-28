d26c6f458931a319cad1fb3553b47c0f41713788
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2022-12-28 08:52:53 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // used to make inform the browser to treat codes as a new version //
+
+//alert( 3 + 3 ); //here we are using nodejs, not a browser so this will not work now
+
+//We can use it as 
+
+console.log( 3 + 3);
+
+console.log("kanak")
+
+
+
+let Name = "kanak"
+let Age = 26
+let isLoggedIn = false
+
+// console.log(isLoggedIn, Age, Name)
+
+// data types in Js
+
+//number => 2 to the power 53//Range from to
+//bigint => used when you hav a large number like for trading and all
+//string => "string here"
+//boolean => True/False.
+//Null    => standalone value
+//undefined => undefined value
+// symboles =>unique
+            // used to identify the uniqueness
+//type of
+console.log(typeof "kanak")
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
