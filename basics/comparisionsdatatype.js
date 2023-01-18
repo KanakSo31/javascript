5bad4392d63f08f82aaa99d94d9a0521c92315c8
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-18 08:20:47 +0000

    comparision datattype complete

---
+++ basics/comparisionsdatatype.js
@@ -0,0 +1,23 @@
+console.log(2 > 3);
+console.log(2 >= 3);
+console.log(2 == 3);
+console.log(2 < 3);
+console.log(2 != 3);
+
+
+console.log("5" > 1);
+console.log("08" < "8");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//==={triple equal check them strictly as shown below}
+console.log("2" === 2);
+//this will not convet the string "2" to a number Nor 2 to a string this will stricly check that wheather the string "2" is equal to the number 2 or not 
