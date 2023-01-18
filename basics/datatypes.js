548938bde210f7f56f9b4d31a3179805cd2728c1
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-18 07:26:13 +0000

    OPERATIONS complete

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -30,3 +30,50 @@ let isLoggedIn = false
 console.log(typeof "kanak")
 console.log(typeof undefined)//undefined
 console.log(typeof null)//object
+
+
+
+//********************************** OPERATIONS **************
+
+let a = 3
+let nega = -a
+console.log(nega);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(5%2);
+// console.log(2/5);
+
+let s1 = "kanak"
+let s2 = " is a front-end engineer"
+let s3 = s1+s2;
+console.log (s3);
+
+// complex situation
+// this operation follows some ecmascript rules to print the output
+console.log("1" + 2);
+console.log("1" + "2");
+console.log(1 + "3");
+console.log("1" + 2 + 2);
+console.log("1" + 2 + "2");
+console.log(1 + 2 + "2");
+
+//console.log(3 + 8 % 5 * 9); //bad way to write code
+// best practices of writing code is using parenthesis
+console.log((3 + 8) % 5 * 9);
+
+
+// should not try to write code like this
+console.log(+true);
+console.log(+"");
+// isted of write like this
+console.log(true);
+console.log(false);
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 8
+console.log(num1);
