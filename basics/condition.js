e52e77567161e2fb8cda9b022d870634fae1c8c3
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-28 13:09:55 +0000

    condition checking

---
+++ basics/condition.js
@@ -0,0 +1,16 @@
+let temperature;
+
+// getting temperature from google
+
+temperature = 20;
+
+if (temperature <= 20){
+    // hardcore the value 
+// if (true){
+    console.log("it's very cool Outside")
+}
+if (temperature <= 30){
+    console.log("it's Moderate Outside")
+}else{
+    console.log("it's very Hot Outside")
+}
