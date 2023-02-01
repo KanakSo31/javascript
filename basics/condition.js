91f5ddc113a318dcec3054580d2ecdad4ec9ab27
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-01 13:42:40 +0000

    login design

--- basics/condition.js
+++ basics/condition.js
@@ -14,3 +14,25 @@ if (temperature <= 30){
 }else{
     console.log("it's Really Hot Outside")
 }
+
+
+
+
+// LogIn conditions
+// Allow user to access course if he is::
+// logged in from email
+// loged in from google
+// logged in from facebook
+
+let email = false;
+let facebook = true;
+let google = true;
+
+
+// write the block of code with if-else statement to pass the aggument passed by the user
+
+
+if(email || facebook || google){
+
+    console.log("Login succesfull");
+}
