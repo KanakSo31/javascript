40a8f45aa2c33894907ce7881b08dec3e787d475
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-02 09:32:31 +0000

    function starting

---
+++ basics/functionbasics.js
@@ -0,0 +1,32 @@
+function sayHello(){
+    console.log("Hey There, Kanak Here");
+}
+
+sayHello()
+sayHello()
+
+// another way
+function sayHello(name){
+    console.log("Hey There, Kanak Here");
+    console.log("Hey There", name);
+}
+
+sayHello("kanak")
+sayHello("himanta")
+
+// best practice
+function sayHello(name){
+    console.log("Hey There, Kanak Here");
+    console.log(`hello there, ${name}. How do you doing ?`);
+}
+
+sayHello("kanak");
+sayHello("himanta");
+
+function nomoskar(){
+    return "Hello in Assam";
+}
+let welcome = nomoskar();
+
+console.log(welcome);
+// also
