127f9dd7600e44b9522c175471da5d3a580bb5eb
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-02 09:00:48 +0000

    values checking

---
+++ basics/values.js
@@ -0,0 +1,14 @@
+// :::FALSY VALUES:::::
+// Undefined
+// Null
+// 0 Zero
+// '' Empty String 
+// NaN not a number
+
+// eg
+
+let user = "0"
+
+if (user){
+    console.log("condition is true");
+}
