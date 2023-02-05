9a7e7c9d6dd3d3ee3cf93569f5a0a399fc48b801
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-05 17:27:00 +0000

    contexts

--- basics/context.js
+++ basics/context.js
@@ -3,3 +3,15 @@
 // function sayHello(){
 //     console.log("Hello");
 // }
+
+if (2==="2") {
+    console.log("this is a posssible true");
+}
+
+
+// this will work on browser 
+
+// let myName = "Kanak";
+// if (muName === window.myname) {
+//     console.log("This is also a true statement");
+// }
