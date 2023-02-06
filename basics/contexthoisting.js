a13ac48065ff7dea35ac4fbce720513dd2dd9422
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-06 12:45:55 +0000

    working with function calling ideas

--- basics/contexthoisting.js
+++ basics/contexthoisting.js
@@ -5,15 +5,32 @@ function tipper(a){
     console.log((bill + 5));
 }
 
-// bigTipper("480")
+// this function will work fine
 
 // function bigTipper(a){
 //     let bill = parseInt(a);
 //     console.log(bill + 5);
 // }
 
+// bigTipper("480")
 
-function bigTipper(a){
+
+
+// this will throw undefined error
+
+// bigTipper("480")
+
+// let bigTipper = function (a){
+//     let bill = parseInt(a);
+//     console.log(bill + 5);
+// }
+
+
+
+// But this will not through any error
+// So Be careful while calling and declearing a function
+
+let bigTipper = function (a){
     let bill = parseInt(a);
     console.log(bill + 5);
 }
