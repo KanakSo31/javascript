2092808974aade419f342ef9d713942e7d976b6d
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-06 12:40:28 +0000

    tipper function

---
+++ basics/contexthoisting.js
@@ -0,0 +1,21 @@
+tipper("5")
+
+function tipper(a){
+    let bill = parseInt(a);
+    console.log((bill + 5));
+}
+
+// bigTipper("480")
+
+// function bigTipper(a){
+//     let bill = parseInt(a);
+//     console.log(bill + 5);
+// }
+
+
+function bigTipper(a){
+    let bill = parseInt(a);
+    console.log(bill + 5);
+}
+
+bigTipper("480")
