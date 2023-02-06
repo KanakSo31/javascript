1663c3475eb984225656806775b21cb1cf74b55a
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-06 13:43:56 +0000

    scope chaining start

--- basics/scopechaining.js
+++ basics/scopechaining.js
@@ -1,9 +1,16 @@
+// IMPORTANT:- Child can ask values from the parent but the parents cannot request the value from the child
 let name = "Kanak"
 
 console.log("Line number 3", name);
 
 function sayKanak() {
-    let name = 
+    let name = "Mr. Kanak"
     console.log("Line Number 7", name);
+    sayKanakTwo();
+
+    function sayKanakTwo(){
+        let name = "MR. KS"
+        console.log("Line number 11", name);
+    }
 }
 sayKanak();
