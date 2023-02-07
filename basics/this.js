049ce8e275f3d6e162fbde8cf4e05702e7de2728
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-07 08:44:45 +0000

    this keyword part 1

---
+++ basics/this.js
@@ -0,0 +1,13 @@
+// TODO: Part One for This keyword
+// this keyword gives us access to the global context and the global context differ the situation
+console.log(this);
+
+let game = "Football";
+
+// function sayName() {
+//     let name = "kanak"
+//     console.log(this);
+    
+// }
+
+sayName();
