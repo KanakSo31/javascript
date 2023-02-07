a839b0e6860e83d4c6dedabf3030162d9c0f9ae8
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-07 11:09:38 +0000

    working with array

---
+++ arrayAndLoop/array.js
@@ -0,0 +1,22 @@
+let countries = ["india", "USA", "Japan", "France", "Russia"];
+
+let states = ["Rajasthan", "Delhi", "Assam", "Mumbai"]
+
+console.log(states[2]);
+
+
+// check the length of an array
+console.log("The array Length is:" + states.length);
+
+// replace an array element
+states[0] = "Punjab"
+console.log(states);
+
+// store the multiple valeus inside arry
+let user = ["kanak", "kanak@gmail.com", 3, 34, true]
+console.log(user);
+
+// deleting one or two value at the end
+user.pop();
+user.pop();
+console.log(user);
