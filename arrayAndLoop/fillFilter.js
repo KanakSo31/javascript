d53ebbef76b5143373077bd47798e827fda5e960
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-07 13:25:50 +0000

    removing and replacing in an array

--- arrayAndLoop/fillFilter.js
+++ arrayAndLoop/fillFilter.js
@@ -1,3 +1,6 @@
 let testArray = [2, 4, 6, 7, 9, 3, 5, 8];
 
-console.log(testArray.fill(1));
+// The value from where the value will be fill is the first element(3) and the second Element(6) is the postion from where it will fill and the 3re element is the end number upto where it will fill.
+// IMPORTAT Is that the starting position is inclusive and the end point is exclusive //it is depends on the language
+
+console.log(testArray.fill("kanak", 3, 6));
