2eb89436cd2faa51b877412afbc9ea49b557454a
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-07 12:38:08 +0000

    arrow function

--- arrayAndLoop/arrayone.js
+++ arrayAndLoop/arrayone.js
@@ -1,8 +1,12 @@
 function isEven(element) {
+    // we can make it a arrow function by adding the => symbol as follows
+// function isEven(element) {
     if (element % 2 === 0) {
         return true;
     }
     return false;
+// we can write it like this as a short form
+// return element % 2 === 0;
 }
 
 console.log(isEven(8));
