a580451996c9b4e9722f95837b821624eb326358
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-07 12:55:25 +0000

    every Function

--- arrayAndLoop/arrayone.js
+++ arrayAndLoop/arrayone.js
@@ -16,5 +16,6 @@ function isEven(element) {
 
 // console.log(isEven(8));
 
+// this check all the numbers ansd id every number is even then print true else false
 let result = [2, 6, 8, 4].every(isEven)
 console.log(result);
