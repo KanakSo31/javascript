b929a20d849ad88cf39db5a0fa1f749f180ab568
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-09 09:36:49 +0000

    objects

---
+++ arrayAndLoop/object.js
@@ -0,0 +1,16 @@
+let user = {
+    firstName : "kanak",
+    lastName : "Sonowal",
+    role : "Admin",
+    loginCount : 32,
+    facebookSignedIn : true
+};
+console.log(user);
+
+console.log(user.firstName);
+console.log(user["lastName"]);
+
+console.log(user.loginCount);
+user.loginCount = 44;
+console.log(user.loginCount);
+console.table(user);
