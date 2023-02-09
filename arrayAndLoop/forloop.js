9d235206cc1deaa23a2efc91bd67e8d2e2c6a139
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-09 13:55:55 +0000

    worked with the while and do while loop

--- arrayAndLoop/forloop.js
+++ arrayAndLoop/forloop.js
@@ -31,3 +31,29 @@ for (let i = 0; i < myStates.length; i++) {
     if(typeof myStates[i] !== 'string') continue;
     console.log(myStates[i]);
 }
+
+console.log("while loop from here");
+// WHILE LOOP
+// let i = 0;
+
+// while(i< myStates.length){
+//     console.log(myStates[i]);
+//     i++;
+// }
+// Do While Loop
+let j = 0;
+do
+    {
+    console.log(myStates[j]);
+    j++;
+    }   
+    while(j< myStates.length);
+
+    // Even if the condition gets false this loop will run once and it will print the i value once
+let k = 200;
+do
+    {
+    console.log(k);
+    k++;
+    }   
+    while(k< 10);
