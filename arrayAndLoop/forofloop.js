723e31a239520bf83ec23557d9aed63c8ecb404d
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-11 20:20:29 +0000

    reading the values from an object using for in loop

--- arrayAndLoop/forofloop.js
+++ arrayAndLoop/forofloop.js
@@ -1,5 +1,20 @@
 let names = ["youtube", "facebook","instagram","Netflix","amazon"]
 
-for(const n of names){
-    console.log(n);
+// for(const n of names){
+//     console.log(n);
+// }
+
+let symbols = {
+    yt: "youtube",
+    ig: "instagram",
+    fb: "facebook",
+    lco: "Learncodeonline",
+    amz: "amazone"
+}
+
+for(const n in symbols ){
+
+    // $symbols[n] is the way of reading the values from an object
+    console.log();
+    console.log(`keys in symbols are: ${n} and values is: ${symbols[n]}`);
 }
