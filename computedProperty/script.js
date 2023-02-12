9bbf87a0cc7b943b82d2bc4952cd07fa52659b60
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-12 22:08:09 +0000

    propertycpmputing

---
+++ computedProperty/script.js
@@ -0,0 +1,15 @@
+var red = document.querySelector(".red");
+let cyan = document.querySelector(".cyan");
+let violet = document.querySelector(".violet");
+let orange = document.querySelector(".orange");
+let pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
+
+// console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGColor = (selectedElement) =>{
+    return window.getComputedStyle(selectedElement).backgroundColor;
+};
+
+console.log(getBGColor(red));
