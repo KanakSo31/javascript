b24a5812d9ddb392eab0c562c2adac442d34fdb8
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-20 12:56:38 +0000

    lexical scoping

--- functions/closerr.js
+++ functions/closerr.js
@@ -10,3 +10,15 @@ function init() {
 var value = init();
 
 value();
+
+
+function doAddition(x) {
+    return function (y){
+        return x + y;
+    };
+}
+
+var add5 = doAddition(4);
+console.log("The Number Is:", + add5(5));
+
+console.log(doAddition(5)(7)); //capability of printing using parenthesis
