88b95a98a6f6473230d71ac1cf5e2852f8adde95
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-20 10:59:14 +0000

    self execution snnonymous function

---
+++ functions/funcone.js
@@ -0,0 +1,20 @@
+// Normal function
+
+// function SayHello() {
+//     console.log("I say hello to You");
+//     console.log("I say hello again");
+// }
+
+// SayHello()
+
+// Self executing anonymous function Or IIFE(immidiately invoked function Expression)
+// Nobody shoul call
+// Shoud have no function name 
+// wrap up with a pair of parenthesis
+
+function SayHello() {
+    console.log("I say hello to You");
+    console.log("I say hello again");
+}
+
+SayHello()
