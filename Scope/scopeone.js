2e4cb1401ad5f67aa90b5d53f19246abe3612051
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-25 13:44:14 +0000

    scoping

---
+++ Scope/scopeone.js
@@ -0,0 +1,11 @@
+// console.log(name);
+
+var name = "kanak";
+
+if (true) {
+    // if you use the var keyword this will not throw an error and if you use let this wil throw an undefined error to the console.
+    // and while using var keyword you can access the lastName outside from the Scope
+    let lastName = "sonowal"
+    console.log(lastName);
+}
+// console.log(lastName);
