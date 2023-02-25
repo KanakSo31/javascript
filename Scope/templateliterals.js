3e4983dc802886f716b1f6b9523f91f15d3c5802
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-02-25 13:52:58 +0000

    template literals

---
+++ Scope/templateliterals.js
@@ -0,0 +1,9 @@
+// let name = "kanak\"s"
+let nameone = `Kanak`;
+let FullName = `${nameone} Sonowal`;
+
+console.log(FullName);
+console.log(FullName.charAt(0));
+console.log(FullName.endsWith("y"));
+console.log(FullName.includes("Kanak"));
+console.log(FullName.toUpperCase());
