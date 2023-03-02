4559a43b743f9cbad35f9b184bd94dff5efd2b68
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-03-02 19:38:42 +0000

    spread and rest

--- Scope/srOperator.js
+++ Scope/srOperator.js
@@ -1,7 +1,14 @@
 let returnedValue = Math.max(2, 2, 5, 7, 4, 2,8);
-console.log(returnedValue);
+// console.log(returnedValue);
 
 let myObj = {};
 
 Object.assign(myObj,{a:1, b:2, c:3}, {z:9, y:6, x:8});
-console.log(myObj);
+// console.log(myObj);
+
+function sumOne(a, b) {
+    return a+b;
+}
+
+let myA = [5, 4]
+console.log(sumOne(...myA));//SPREAD operator..
