179f7921256231f5a7615c1d660ade2bc946c593
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-03-03 09:58:08 +0000

    working with API

--- Scope/API.js
+++ Scope/API.js
@@ -1,9 +1,13 @@
 fetch("https://api.chucknorris.io/jokes/random")
 .then((response) =>{
-    console.log(response);
+    return response.json();
+})
+.then((data)=>{
+    // console.log("data is:", data);
+    let joke = data.value;
+    console.log("Joke Of Today:", joke);
 })
 .catch();
-
 
 // scenerio is like :
 // try {
